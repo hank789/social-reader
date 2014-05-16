5c070a6f0a0b39db0978b49417d5182ff47dab9b
hank <hank.wang@dokec.com>
2014-05-16 12:08:17 +0800

    生产环境初始化管理员

--- db/fixtures/production/001_admin.rb
+++ db/fixtures/production/001_admin.rb
@@ -1,5 +1,5 @@
 admin = User.create(
-  email: "admin@local.host",
+  email: "poqbod@poqbod.com",
   name: "Administrator",
   username: 'root',
   password: "12345678",
@@ -14,18 +14,11 @@ admin.admin = true
 admin.save!
 admin.confirm!
 
-Project.seed(:id, [
-    { id: 1,  name: "To-Do", slug: 'todo', description: "", creator_id: "1" },
-    { id: 2,  name: "Important", slug: 'importmant', description: "", creator_id: "1" },
-    { id: 3,  name: "Normal", slug: 'normal', description: "", creator_id: "1" },
-    { id: 4,  name: "Low", slug: 'low', description: "", creator_id: "1" },
-])
-
 if admin.valid?
 puts %q[
 Administrator account created:
 
-login.........admin@local.host
-password......5iveL!fe
+login.........poqbod@poqbod.com
+password......12345678
 ]
 end
