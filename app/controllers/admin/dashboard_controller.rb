97a29d5f891034fab3820d46a69c43623f1f59ae
hank <hank.wang@dokec.com>
2014-05-29 18:26:05 +0800

    fix #88 优化后台dashboard显示和计数

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,8 @@
 class Admin::DashboardController < Admin::ApplicationController
   def index
-    @services = Service.order("created_at DESC").limit(10)
-    @users = User.order("created_at DESC").limit(10)
+    @services = Service.order("created_at DESC")
+    @users = User.order("created_at DESC")
+    @rss_feeds = RssFeed.all
     #@groups = Group.order("created_at DESC").limit(10)
   end
 end
