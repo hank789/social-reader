103b17590b301e9e39ad8b26e44d47f6a0374371
hank <hank.wang@dokec.com>
2014-05-29 18:32:22 +0800

    #88 只显示最近的15条

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class Admin::DashboardController < Admin::ApplicationController
   def index
-    @services = Service.order("created_at DESC")
-    @users = User.order("created_at DESC")
+    @services = Service.order("created_at DESC").limit(15)
+    @users = User.order("created_at DESC").limit(15)
     @rss_feeds = RssFeed.all
     #@groups = Group.order("created_at DESC").limit(10)
   end
