f2a91a59dfb9fa2ee7ed18a3c819c5c9cac5ae91
hank <hank.wang@dokec.com>
2014-06-03 20:58:59 +0800

    收藏页面和发现页面不出现标记为已读的按钮

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,7 +14,7 @@ class EventsController < ApplicationController
 
   def mark_above_as_read
     lasted_event = Event.find(cookies['lasted_event_id'])
-    Event.where("created_at >= ? and created_at <= ?",@event.created_at,lasted_event.created_at).update_all(action: Event::READ)
+    Event.where("user_id = ? and created_at >= ? and created_at <= ?",current_user.id, @event.created_at,lasted_event.created_at).update_all(action: Event::READ)
     respond_to do |format|
       format.js {render inline: "$('.content_list').html('');Pager.init(50, true);" }
     end
