1e48b71b9adf93e9145da9b8bc0d9de4389f70b9
hank <hank.wang@dokec.com>
2014-06-06 23:18:53 +0800

    修复标记为已读的bug，只对当前选得优先级或service标记优先级

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,8 +13,22 @@ class EventsController < ApplicationController
   end
 
   def mark_above_as_read
+    filter = []
+    if cookies['event_filter'].present?
+      filter = cookies['event_filter'].split(',')
+    end
+    actions = []
+    actions << Service::IMPORTANT if filter.include? 'important'
+    actions << Service::NORMAL if filter.include? 'normal'
+    actions << Service::LOW if filter.include? 'low'
+
+    if actions.empty?
+      services_ids = filter[0].to_i
+    else
+      services_ids = Service.where(priority: actions, user_id: current_user.id).pluck(:id)
+    end
     lasted_event = Event.find(cookies['lasted_event_id'])
-    Event.where("user_id = ? and created_at >= ? and created_at <= ?",current_user.id, @event.created_at,lasted_event.created_at).update_all(action: Event::READ)
+    Event.where(service_id: services_ids, user_id: current_user.id, created_at: @event.created_at..lasted_event.created_at).update_all(action: Event::READ)
     respond_to do |format|
       format.js {render inline: "$('.content_list').html('');Pager.init(50, true);" }
     end
